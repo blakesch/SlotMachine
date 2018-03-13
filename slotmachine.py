1821270ee75329085eec355d4eafc4f53aef5c39
blakesch <37336586+blakesch@users.noreply.github.com>
2018-03-13 11:56:24 -0500

    formatting changes

--- slotmachine.py
+++ slotmachine.py
@@ -4,29 +4,34 @@
 print("Welcome to the Slot Machine Simulator")
 print("To win the jackpot you need to have 3 cherries in a row.")
 print("You can play up to 100 rounds at once. Select a number between 1 and 100")
-games = input("How many games would you like to run?")
+games = input("How many games would you like to run? ")
 slotsPossible = ("apple", "apple", "apple", "cherry", "crown", "lemon")
 loser = ("\nYou win absolutely nothing", "\nFeed me more money", "\nYou lost, try again")
+
 from random import *
 if int(games)<1 :
     print ("You didn't deserve to play anyway!")
 if int(games)>100 :
     print ("Sorry you can only play 100 games so 100 is all you get!")
     games = 100
+
+
 def play():
-    slot1=choice (slotsPossible)
-    slot2=choice (slotsPossible)
-    slot3=choice (slotsPossible)
+    slot1=choice(slotsPossible)
+    slot2=choice(slotsPossible)
+    slot3=choice(slotsPossible)
     lose = choice(loser)
     win = lose
-    if(slot1==slot2==slot3=="cherry"):
+    if(slot1 == slot2 == slot3 == "cherry"):
         win = "\n\nJackpot! You're a big winner! $500!\n"
-    if(slot1==slot2==slot3=="crown"):
+    if(slot1 == slot2 == slot3 == "crown"):
         win = "\n\nYou won $100!\n"
-    if(slot1==slot2==slot3=="apple"):
+    if(slot1 == slot2 == slot3 == "apple"):
         win = "\n\nYou win a small prize: $10!\n"
-    if(slot1==slot2==slot3=="lemon"):
+    if(slot1 == slot2 == slot3 == "lemon"):
         win = "\n\nTime to make some lemonade!\n"
     return slot1+":"+slot2+":"+slot3+" "+win
-for i in range(int(games)) :
+
+
+for i in range(int(games)):
     print(play())
